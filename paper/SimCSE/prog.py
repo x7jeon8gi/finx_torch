e4cd9583370e28d02a48a35b0016b44b43669cae
DuHyen <shjjk21@gmail.com>
2022-06-03 21:11:55 +0900

    settings에 주석작업

--- paper/SimCSE/prog.py
+++ paper/SimCSE/prog.py
@@ -1,11 +1,28 @@
 import argparse
 
-parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument('integers', metavar='N', type=int, nargs='+',
+# 실행하고 싶으면, .py로 만들고 shell에서 실행해야 argparse가 사용되는 모습을 볼 수 이씀
+# 현재 파일명은 prog.py이고 shell에서 python prog.py를 실행해야한다
+
+parser = argparse.ArgumentParser(description ='Process some integers.')
+
+# python prog.py -h 이러한 명령어를 실행하게 되면, description 처럼 str 형태로 저장해놓은 놈들이 출력된다.
+# 각각의 argument마다 설명을 추가할 수 있으며, -h 명령을 실행하면 나타나게 됨.
+# 내부에 들어가는 파라미터에 대한 것은 일단 생략
+
+# 정리하면, argparse는 shell에서 model에 어떤 인자들을 넘겨주고 싶을 때 사용하는 함수이다.
+
+parser.add_argument("integers", metavar='N', type=int,nargs='+',
                     help='an integer for the accumulator')
+
 parser.add_argument('--sum', dest='accumulate', action='store_const',
                     const=sum, default=max,
                     help='sum the integers (default: find the max)')
 
+                    # help는 각 명령어에 대한 설명을 넣어주는 것.
+                    # add_argument를 하는 것은 어떻게 하는 거징? 안에 있는 파라미터들이 어떻게 구성되는건지 잘 모르게따.
+
+
 args = parser.parse_args()
 print(args.accumulate(args.integers))
+
+#
--- paper/SimCSE/settings.py
+++ paper/SimCSE/settings.py
@@ -5,18 +5,24 @@ import logging
 import numpy as np
 from argparse import ArgumentParser
 
+# https://greeksharifa.github.io/references/2019/02/12/argparse-usage/
+# argparse에 대한 간단하고 풍부한 설명이 있으니 참고!
+# argument들을 전부 지정하지 않더라도, default값이 있으니 ㄱㅊ
 
 class Arguments():
 
     def __init__(self):
         self.parser = ArgumentParser()
 
-    def add_type_of_processing(self):
+    def add_type_of_processing(self): # parser에다가 argument를 추가해주는 함수인데...
         self.add_argument('--opt_level', type=str, default='O1')
         self.add_argument('--fp16', type=str, default='True')
         self.add_argument('--train', type=str, default='True')
         self.add_argument('--test', type=str, default='True')
         self.add_argument('--device', type=str, default=torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu'))
+        
+        # cuda 사용 가능시, device를 cuda, 그게 아니면,cpu 사용
+        # self.add_argument -> 객체 생성 후, 메서드로 사용됨.
 
     def add_hyper_parameters(self):
         self.add_argument('--patient', type=int, default=10)
@@ -31,6 +37,8 @@ class Arguments():
         self.add_argument('--warmup_ratio', type=float, default=0.05)
         self.add_argument('--temperature', type=float, default=0.05)
 
+        # 필요한 하이퍼 파라미터들.
+
     def add_data_parameters(self):
         self.add_argument('--train_data', type=str, default='train_nli.tsv')
         self.add_argument('--valid_data', type=str, default='valid_sts.tsv')
@@ -41,6 +49,8 @@ class Arguments():
         self.add_argument('--path_to_saved_model', type=str, default='./output/')
         self.add_argument('--ckpt', type=str, default='best_checkpoint.pt')
 
+        # 학습이나 모델 운영시 사용하게 될 data들
+
     def print_args(self, args):
         for idx, (key, value) in enumerate(args.__dict__.items()):
             if idx == 0:print("argparse{\n", "\t", key, ":", value)
@@ -50,12 +60,19 @@ class Arguments():
     def add_argument(self, *args, **kw_args):
         return self.parser.add_argument(*args, **kw_args)
 
+        # 한번에 여러개의 입력을 받을 수 있게 해주는 것으로 보임.
+
     def parse(self):
         args = self.parser.parse_args()
         self.print_args(args)
 
         return args
+        # 명령창에서 주어진 인자를 파싱할 수 있도록 해줌
 
+
+        # 어렴풋이는 argparse의 목적에 대해서 이해했으나, 어디서 어떤 식으로 사용이 되는지는 아직 잘 모르겠다.
+        # 계속 진행하면서 파악해보도록 한다.
+        # args는 뭐하는 쉐끼냐...
 
 class Setting():
 
@@ -64,13 +81,14 @@ class Setting():
         _logger = logging.getLogger()
         formatter = logging.Formatter(
             '[%(levelname)s] %(asctime)s [ %(message)s ] | file::%(filename)s | line::%(lineno)s')
+            # 오류 발생시, 어떤 형태로 오류를 출력할지 설정해주는 객체
 
         stream_handler = logging.StreamHandler()
         stream_handler.setFormatter(formatter)
 
         _logger.addHandler(stream_handler)
         _logger.setLevel(logging.DEBUG)
-
+        # debug 수준의 낮은 오류는 따로 파일로 만들지 않고, 그냥 로그에 남긴다.
         return _logger
 
     def set_seed(self, args):
@@ -87,6 +105,8 @@ class Setting():
         torch.cuda.manual_seed(seed)
         torch.cuda.manual_seed_all(seed)
 
+    # seed 설정
+
     def run(self):
 
         parser = Arguments()
@@ -100,5 +120,4 @@ class Setting():
         self.set_seed(args)
 
         return args, logger
-    
 
